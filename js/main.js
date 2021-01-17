5317a6ede3f9592a0b7140e9dfb5577239021179
Diago <57977686+panjidiago27@users.noreply.github.com>
2021-01-17 07:35:09 +0700

    update

--- js/main.js
+++ js/main.js
@@ -24,19 +24,6 @@ $(document).ready(function(){
 //==============___Page Loader___================
   $('#loading-wraper').fadeIn(300);
 
-//==============___Testimonials - owl Carousel___================
- $("#testimonial-carousel").owlCarousel({
-    navigation : false, // Show next and prev buttons
-    slideSpeed : 300,
-    paginationSpeed : 400,      
-    responsiveRefreshRate : 200,
-    responsiveBaseWidth: window,
-    pagination: true,
-    singleItem: true,    
-    navigationText: ["<span class='fa fa-chevron-left'></span>","<span class='fa fa-chevron-right'></span>"],     
-  });
-
-
 //==============_Map_================
 $('.map').on('click', function(){
 	$('.map-overlay').hide();
@@ -44,16 +31,6 @@ $('.map').on('click', function(){
 
 $('.map').on('mouseleave', function(){
 	$('.map-overlay').show();
-});
-
-//==============_Lightbox_================
-//Nivo Lightbox
-  $('a.nivobox').nivoLightbox({ effect: 'fade' });
-
-
-//==============___Scrollbars___================
-$('.section-vcardbody').perfectScrollbar({
-  wheelSpeed: 0.9
 });
 
 //==============___Menu & Pages Animation___================
@@ -126,47 +103,6 @@ $(".loadPost").on('click', function(event){
   pageOn();
   loadPost();
 });
-
-//==============___Contact Form Validator and Ajax Sender___================
-  $("#contactForm").validate({
-    submitHandler: function(form) {
-      $.ajax({
-        type: "POST",
-        url: "php/contact-form.php",
-        data: {
-          "name": $("#contactForm #name").val(),
-          "email": $("#contactForm #email").val(),
-          "subject": $("#contactForm #subject").val(),
-          "message": $("#contactForm #message").val()
-        },
-        dataType: "json",
-        success: function (data) {
-          if (data.response == "success") {
-            $("#contactSuccess").fadeIn(300);
-            $("#contactError").addClass("hidden");
-
-            $("#contactForm #name, #contactForm #email, #contactForm #subject, #contactForm #message")
-              .val("")
-              .blur()
-              .closest(".control-group")
-              .removeClass("success")
-              .removeClass("error");              
-            
-          } else {
-            $("#contactError").fadeIn(300);
-            $("#contactSuccess").addClass("hidden");
-          }
-        }
-
-      });
-    }
-  });
-
-
-//Modal for Contact Form
-$('.modal-wrap').click(function(){
-  $('.modal-wrap').fadeOut(300);
-});   
 
 //End - Document Ready
 });
